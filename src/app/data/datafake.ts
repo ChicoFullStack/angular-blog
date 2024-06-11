ef5f195b1b295b72d494fa746de67c0399505cf8
chicofullstack <chico_fullstack_2023@outlook.com>
2024-06-11 00:17:45 -0300

    content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"IRON MAN NOS CINEMAS",
+    "description":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident explicabo similique quos! Dicta et, fugiat odit molestias quos aut obcaecati dolore voluptatibus. Sed exercitationem omnis repellendus. Aspernatur labore velit et!",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/3421528F5E3679CEA7D89FE51BE6DE6904289364AD148688A2E236A340144BF6/scale?width=1200&aspectRatio=1.78&format=webp"
+  },
+  {
+    "id":"2",
+    "title":"saiu novo filme",
+    "description":"ola",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title":"saiu novo filme",
+    "description":"ola",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtpzrhviSEQ9g4kXQz23tnDDcKBspQILK1eQ&s"
+  },
+  {
+    "id":"4",
+    "title":"saiu novo filme",
+    "description":"ola",
+    "photoCover":"https://podpop.com.br/PodPOP/wp-content/uploads/2020/10/s%C3%A9ries-da-Marvel-752x440.jpg"
+  },
+];
